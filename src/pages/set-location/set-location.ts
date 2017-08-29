2b6d2c73e2c3706e1d79bf04d28696bacf90631e
joshgressman <joshgressman@gmail.com>
2017-08-29 15:14:58 -0500

    added location marker

--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -8,12 +8,15 @@ import { Location } from '../../models/location';
 })
 export class SetLocationPage {
    location: Location;
-
+   marker: Location;
   //accessing navigation object set in params from add-place page
   constructor(private navParams: NavParams){
     this.location = this.navParams.get('location');
   }
   //map location will be handled by an Angular package
   //   npm install - - save @agm/core
-
+  onSetMarker(event: any){
+    this.marker = new Location(event.coords.lat, event.coords.lng)
+    console.log(this.marker);
+  }
 }
