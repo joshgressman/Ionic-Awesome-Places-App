fcaaf824f011b062fa06d01445e511299049120f
joshgressman <joshgressman@gmail.com>
2017-09-04 20:22:47 -0500

    added place content

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
 import { AddPlacePage } from '../add-place/add-place'
 import { Place } from '../../models/place';
 import { PlacesService } from '../../services/places';
+import { PlacePage } from '../place/place';
 
 @Component({
   selector: 'page-home',
@@ -12,11 +13,17 @@ export class HomePage {
   addPlacePage = AddPlacePage;
   places: Place[] = [];
 
-  constructor(public navCtrl: NavController, private placesService: PlacesService) {
+  constructor(public modalCtrl: ModalController, private placesService: PlacesService) {
 
   }
 
   ionViewWillEnter(){
     this.places = this.placesService.loadPlace();
+  }
+
+  //will open the place page in a modal with the idevidual place data passed within the object
+  onOpenPlace(place: Place){
+   const modal = this.modalCtrl.create(PlacePage, {place: place});
+   modal.present();
   }
 }
--- src/pages/place/place.ts
+++ src/pages/place/place.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-
+import { NavParams, ViewController } from 'ionic-angular';
+import { Place } from '../../models/place';
 
 @Component({
   selector: 'page-place',
   templateUrl: 'place.html',
 })
 export class PlacePage {
+   place: Place;
+ constructor(private navParams: NavParams, private viewCtrl: ViewController){
+   this.place = this.navParams.get('place');
+ }
 
+ onLeave(){
+  this.viewCtrl.dismiss();
+ }
 
-
-
+ onDelete(){
+   
+   this.onLeave();
+ }
 
 }
