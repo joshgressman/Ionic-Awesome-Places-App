dc1452c866a09a14c49d8cc12846ad3c6d3b1384
joshgressman <joshgressman@gmail.com>
2017-09-06 19:10:37 -0500

    added storage removal

--- src/services/places.ts
+++ src/services/places.ts
@@ -3,12 +3,13 @@ import { Location } from '../models/location';
 import { Injectable } from '@angular/core';
 //ionic storage
 import { Storage } from '@ionic/storage';
+import { File } from '@ionic-native/file';
 
 @Injectable()
 export class PlacesService {
   private places: Place[] = [];
 
-  constructor(private storage: Storage){}
+  constructor(private storage: Storage, private file: File){}
 
   addPlace(title: string, description: string, location: Location, imageUrl: string){
    const place = new Place(title, description, location, imageUrl);
@@ -48,7 +49,40 @@ export class PlacesService {
   }
 
   deletePlace(index: number){
+    const place = this.places[index];
     this.places.splice(index, 1);
+    //remove from array in service then update within Storage to update new array
+    this.storage.set('places', this.places)
+    //removed the saved image within file in .then()
+    .then(
+      () => {
+        //this is set to inactive due to not running with a camera and not saving files
+      //  this.removeFile(place);
+      }
+    )
+    .catch(
+      err => {
+        console.log(err);
+      }
+    )
+
+  }
+
+  private removeFile(place: Place){
+     const currentName = place.imagePath.replace(/^.*[\\\/]/,'');
+     this.file.removeFile(this.file.dataDirectory, currentName)
+     .then(
+       () => {
+         console.log("file removed");
+       }
+     )
+     .catch(
+       () => {
+         console.log("error while removing file");
+         //if removing is not successful add place back to places[]
+         this.addPlace(place.title, place.description, place.location, place.imagePath);
+       }
+     )
   }
 
 }
